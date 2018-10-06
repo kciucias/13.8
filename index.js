f514e83467561dae36abb8ba107a6b4aa864e726
Katarzyna Ciucias <kasia.ciucias@gmail.com>
2018-10-06 12:21:05 +0100

    finish

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ server.on('request', function (request, response) {
 
     if (request.method === 'GET' && (request.url === '/url')) {
         response.setHeader("Content-Type", "text/html; charset=utf-8");
-        fs.readfile('./index.html', 'utf-8', function(err, data){
+        fs.readFile('./index.html', 'utf-8', function(err, data){
             response.write(data);
             response.end();
         });
